8b6e8efda1f9995e3923b9b9c4a4a3333456b344
Alessandro Pramudhita Putra Setyawan <103131101+aledhita02@users.noreply.github.com>
2023-11-20 22:25:59 +0700

    Delete app directory

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL','http://localhost:8080/belajar/phpmvc/public');
-
-//DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'phpmvc');
